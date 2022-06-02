891c892b04d411bb227f15b2c7afad80161512d2
ImranRH321 <imranrh247@gmail.com>
2022-06-02 18:31:14 +0600

    all-ready-exists-display-error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import "./App.css";
 import app from "./firebase.init";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { Button, Form } from "react-bootstrap";
 
 const auth = getAuth(app);
 
 function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [validated, setValidated] = useState(false);
+  const [error, setError] = useState("");
 
   const handleEmailBlur = event => {
     setEmail(event.target.value);
@@ -18,51 +22,73 @@ function App() {
   };
 
   const handleFormSubmit = event => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      return;
+    }
+    if (!/(?=.*?[#?!@$%^&*-])/.test(password)) {
+      setError("password is one specail carekter");
+      return;
+    }
+    setError("");
+    setValidated(true);
+    // ..
     createUserWithEmailAndPassword(auth, email, password)
       .then(result => {
         const user = result.user;
         console.log(user);
+        setEmail("");
+        setError("");
       })
       .catch(error => {
-        console.log(error);
+        console.error(error);
+        setError(error.message);
       });
     event.preventDefault();
   };
-
+  console.log(error);
   return (
-    <div className="text-center container">
+    <div className="container w-50 mx-auto">
       <h1>Register Login</h1>
-      <form onSubmit={handleFormSubmit}>
-        <div class="">
-          <label for="staticEmail" class="col-sm-2 col-form-label ">
-            Email
-          </label>
-          <div>
-            <input
-              onBlur={handleEmailBlur}
-              type="text"
-              readonly
-              className="flied"
-              id="staticEmail"
-            />
-          </div>
-        </div>
-        <div class="">
-          <label for="inputPassword" class="col-sm-2 col-form-label">
-            Password
-          </label>
-          <div class="">
-            <input
-              onBlur={handlePasswordBlur}
-              type="password"
-              className="flied"
-              id="inputPassword"
-            />
-          </div>
-        </div>
-        <br />
-        <button type="submit">Sign in</button>
-      </form>
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            onBlur={handleEmailBlur}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+          <Form.Control.Feedback type="invalid">
+            Please choose a email.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            onBlur={handlePasswordBlur}
+            type="password"
+            placeholder="Password"
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            password one specail simple ...
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <h3 className="text-danger">{error}</h3>
+        <Button className="btn" variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
